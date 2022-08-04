e215207a187eab60ffdac9a3721eaea1079304ef
Jakin-Kim <kimyagin94@gmail.com>
2022-08-05 00:27:02 +0900

    Lifting State Up done in Completing the Game part

--- src/index.js
+++ src/index.js
@@ -3,24 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 // Square component renders a single <button>.
-class Square extends React.Component {
-  constructor(props) {
-    super(props); // 부모 클래스의 생성자(constructor)에 넘겨줄 매개변수가 있는 경우, super()를 써서 부모 클래스의 생성자를 매개변수로 넘겨준다.    
-    this.state = { // React components can have 'state(got clicked and filled it with an "X" mark)' by setting 'this.state' in their constructors.
-      value: null,
-    }; 
-  }
+class Square extends React.Component { // As of lifting state up, Square components are now 'controlled components'.
+  // constructor(props) {
+  //   super(props); // 부모 클래스의 생성자(constructor)에 넘겨줄 매개변수가 있는 경우, super()를 써서 부모 클래스의 생성자를 매개변수로 넘겨준다.    
+  //   this.state = { // React components can have 'state(got clicked and filled it with an "X" mark)' by setting 'this.state' in their constructors.
+  //     value: null,
+  //   }; 
+  // }
 
   render() {
     return (
       <button 
         className="square"
-        onClick={() => this.setState({value: 'X'})}
-        // By calling 'this.setState' from an 'onClick' handler in the Square’s 'render' method, 
+        onClick={() => this.props.onClick()}
+        // By calling 'this.setState' from an 'onClick' handler in the Square’s 'render()' method, 
         // we tell React to re-render that Square whenever its <button> is clicked.
       >
         {/* Receiving data from parent component */}
-        {this.state.value}
+        {this.props.value}
+        
       </button>
     );
   }
@@ -29,8 +30,25 @@ class Square extends React.Component {
 
 // Board component renders 9 squares.
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice(); // copy of existing 'squares' array
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
   renderSquare(i) {
-    return <Square value={i} />; // passing props to child component
+    return (
+      <Square 
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+       />); // passing props to child component
   }
 
   render() {
@@ -80,6 +98,7 @@ class Game extends React.Component {
 
 // ========================================
 
+// Over React v18.
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Game />);
 
